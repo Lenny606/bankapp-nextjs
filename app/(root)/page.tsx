f841e483e4a9d7c8b5aa7257168441c6bf88208d
tomas kravcik <kravcik@procorp.cz>
2024-11-04 19:22:16 +0100

    HeaderBar.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
+import {HeaderBar} from '@/components/HeaderBar'
 import React from 'react'
+
 const Home = () => {
+    const loggedIn = {
+        firstname: "Tomas"
+    }
     return (
-        <>Home</>
+        <section className={'home'}>
+            <div className={'home-content'}>
+                <div className={'home-header'}>
+                    <HeaderBar
+                        type={'greeting'}
+                        title={'Welcome'}
+                        subtext={"Access and manage your account"}
+                        user={loggedIn?.firstname || "Guest" }
+                    />
+                </div>
+            </div>
+        </section>
     )
 }
 export default Home
