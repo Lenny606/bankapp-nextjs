e098545bfe859a0df1a7faf3f48769878d2157e3
Tomas Kravcik <root@Laptop-i7>
2024-11-09 15:13:33 +0100

    new routes

---
+++ app/(root)/my-banks/page.tsx
@@ -0,0 +1,13 @@
+const MyBanks = () => {
+    const loggedIn = {
+        firstName: "Tomas",
+        lastName: "Smith",
+        email: "tom@example.com"
+    }
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default MyBanks
---
+++ app/(root)/payment-transfer/page.tsx
@@ -0,0 +1,10 @@
+const PaymentTransfer = () => {
+
+    }
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default PaymentTransfer
