2d72562fe1e19cb3cbadf7012977abf0a5c90602
Tomas Kravcik <root@Laptop-i7>
2024-11-19 20:46:47 +0100

    dynamic names in dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,15 @@ import {HeaderBar} from '@/components/HeaderBar'
 import React from 'react'
 import {TotalBalanceBox} from "@/components/TotalBalanceBox";
 import {RightSidebar} from "@/components/RightSidebar";
+import {getLoggedInUser} from "@/lib/actions/user.actions";
 
-const Home = () => {
-    const loggedIn = {
-        firstName: "Tomas",
-        lastName: "Smith",
-        email: "tom@example.com"
-    }
+const Home = async () => {
+    // const loggedIn = {
+    //     firstName: "Tomas",
+    //     lastName: "Smith",
+    //     email: "tom@example.com"
+    // }
+    const loggedIn = await getLoggedInUser()
     return (
         <section className={'home'}>
             <div className={'home-content'}>
@@ -17,7 +19,7 @@ const Home = () => {
                         type={'greeting'}
                         title={'Welcome'}
                         subtext={"Access and manage your account"}
-                        user={loggedIn?.firstname || "Guest" }
+                        user={loggedIn?.name || "Guest" }
                     />
                     <TotalBalanceBox
                     accounts={[]}
